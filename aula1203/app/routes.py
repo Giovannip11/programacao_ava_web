66223f4d702a773de2ef75dc9767292f1cddde83
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2025-03-12 10:46:15 -0300

    Add files via upload

---
+++ aula1203/app/routes.py
@@ -0,0 +1,31 @@
+from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user={'username': 'Giggio'}
+    posts = [
+        {
+            'author': {'username': 'Joao'},
+            'body': 'Belo dia em Vila Velha!'
+        },
+        {
+            'author': {'username': 'Maria'},
+            'body': 'Partiu Carnaval!'
+        }
+
+    ]
+
+    return render_template('index.html',title="Home", user=user, posts=posts)
+
+@app.route('/login', methods= ['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login solicitado pelo usuario {}, Remember Me = {}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('Login.html',title="Sign In", form=form)
+        
